44277e974d7e62642db2b4250b2294f65a5063ae
Laureano Clausi <laureano.clausi@gmail.com>
2019-07-21 16:24:32 -0300

    cambio el dominio

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,28 @@
+
+delete from cuenta_periodo;
+delete from cuenta;
+delete from estado;
+delete from producto;
+delete from proyecto;
+
 insert into proyecto
 (id , nombre        ) values 
 (1  , 'Proyecto 1'  );
--- 
--- insert into producto 
--- (id, nombre  , precio, proyecto_id) values 
--- (1 , 'Lentes', 500.0 , 1          );
--- 
--- 
--- insert into estado
--- (activo, caja  , costo_fijo, costo_variable, mes, produccion_mensual, producto_id, stock, parametros_ventas_desvio, parametros_ventas_media ) values
--- (true  , 11500.0,200.0     , 3.5           , 1  , 150               , 1          , 200  , 0.10                     , 180);
--- 
--- insert into cuenta
--- (id, descripcion  , tipo_cuenta   ,TIPO_FLUJO_FONDO, proyecto_id) values
--- (1 , 'Caja'       , 'FINANCIERO'  , null           ,1);
--- 
--- insert into cuenta_periodo
--- (id,cuenta_id, monto, periodo) values
--- (1,1         , 500.0,1       ),
--- (2,1         , 400.0,2       );
+
+insert into producto 
+(id, nombre  , precio, proyecto_id) values 
+(1 , 'Lentes', 500.0 , 1          );
+
+
+insert into estado
+(activo, caja  , costo_fijo, costo_variable, mes, produccion_mensual, producto_id, stock, parametros_ventas_desvio, parametros_ventas_media ) values
+(true  , 11500.0,200.0     , 3.5           , 1  , 150               , 1          , 200  , 0.10                     , 180);
+
+insert into cuenta
+(id, descripcion  , tipo_cuenta   ,TIPO_FLUJO_FONDO, proyecto_id) values
+(1 , 'Caja'       , 'FINANCIERO'  , null           ,1);
+
+insert into cuenta_periodo
+(id,cuenta_id, monto, periodo) values
+(1,1         , 500.0,1       ),
+(2,1         , 400.0,2       );
