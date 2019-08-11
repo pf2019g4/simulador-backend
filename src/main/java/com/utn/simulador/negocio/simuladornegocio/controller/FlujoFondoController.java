74159087583864ab890e6c9b0bae14bc5a0e934c
fede22 <fede22>
2019-08-11 20:15:55 -0300

    agrego endpoint rest para flujo de fondo. 1ra versión

---
+++ src/main/java/com/utn/simulador/negocio/simuladornegocio/controller/FlujoFondoController.java
@@ -0,0 +1,30 @@
+package com.utn.simulador.negocio.simuladornegocio.controller;
+
+
+import com.utn.simulador.negocio.simuladornegocio.service.FlujoFondoService;
+import com.utn.simulador.negocio.simuladornegocio.vo.AgrupadorVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@CrossOrigin
+public class FlujoFondoController {
+
+    private final FlujoFondoService flujoFondoService;
+
+    @GetMapping("/proyecto/{id}/flujo-fondo")
+    public Map<String, AgrupadorVo> obtenerFlujoFondo(@PathVariable("id") Long id){
+
+        return flujoFondoService.calcularCuentas(id,5,Long.valueOf(0));
+
+
+    }
+
+
+
+}
