ff7213a549ddc654dfee1159c6179bf5e5135db7
Laureano Clausi <laureano.clausi@gmail.com>
2019-08-18 19:07:24 -0300

    arreglo merge

--- src/test/java/com/utn/simulador/negocio/simuladornegocio/service/DecisionServiceTest.java
+++ src/test/java/com/utn/simulador/negocio/simuladornegocio/service/DecisionServiceTest.java
@@ -3,7 +3,6 @@ package com.utn.simulador.negocio.simuladornegocio.service;
 import com.utn.simulador.negocio.simuladornegocio.SimuladorNegocioApplicationTests;
 import com.utn.simulador.negocio.simuladornegocio.builder.*;
 import com.utn.simulador.negocio.simuladornegocio.domain.*;
-import com.utn.simulador.negocio.simuladornegocio.repository.DecisionRepository;
 import com.utn.simulador.negocio.simuladornegocio.vo.DecisionVo;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +15,6 @@ public class DecisionServiceTest extends SimuladorNegocioApplicationTests {
     @Autowired
     private DecisionService decisionService;
 
-    @Autowired
-    private DecisionRepository decisionRepository;
-
     @Test
     public void obtenerPorProyecto_porProyectoValido_devuelveDecisionesConSusRespuestasYConsecuencias() {
 
@@ -27,24 +23,7 @@ public class DecisionServiceTest extends SimuladorNegocioApplicationTests {
         List<DecisionVo> decisiones = decisionService.obtenerPorProyecto(proyecto.getId());
 
         assertThat(decisiones).hasSize(0);
-//        assertThat(decisiones.get(0).getRespuestas()).hasSize(2);
-//        assertThat(decisiones.get(0).getRespuestas().get(0).getConsecuencias()).hasSize(1);
-//        assertThat(decisiones.get(0).getRespuestas().get(0).getConsecuencias().get(0).getCuentaId()).isEqualTo(cuenta.getId());
 
-    }
-
-    @Test
-    public void guardar_conDecisionNueva_guardaEnBD() {
-//
-//        Proyecto proyecto = ProyectoBuilder.proyectoAbierto().build(em);
-//
-//        Decision decision = new Decision();
-//        decision.setDescripcion("Desc1");
-//        decision.setProyectoId(proyecto.getId());
-//
-//        decisionService.guardar(decision);
-//
-//        assertThat(decisionRepository.count()).isEqualTo(1);
     }
 
 }
