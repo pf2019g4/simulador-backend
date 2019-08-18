f551786d83f73ec2f61c472c77b96837076cfa6a
Will <shn@MacBook-Air-de-William-3.local>
2019-08-18 16:17:09 -0300

    agrego notaciones para evitar problemas con json

--- src/main/java/com/utn/simulador/negocio/simuladornegocio/domain/CuentaPeriodo.java
+++ src/main/java/com/utn/simulador/negocio/simuladornegocio/domain/CuentaPeriodo.java
@@ -1,5 +1,6 @@
 package com.utn.simulador.negocio.simuladornegocio.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -20,6 +21,7 @@ public class CuentaPeriodo {
     private Long id;
     @ManyToOne
     @JoinColumn(name = "cuenta_id", nullable = false)
+    @JsonBackReference
     private Cuenta cuenta;
     private BigDecimal monto;
 
