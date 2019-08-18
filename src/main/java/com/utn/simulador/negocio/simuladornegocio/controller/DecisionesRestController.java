e58d7c2b5f424bec5dd27fa914fa80c437de6e54
shgarcia <shgarcia@tgvamericas.net>
2019-08-18 16:33:20 -0300

    cross origin + fix json decisiones

--- src/main/java/com/utn/simulador/negocio/simuladornegocio/controller/DecisionesRestController.java
+++ src/main/java/com/utn/simulador/negocio/simuladornegocio/controller/DecisionesRestController.java
@@ -5,18 +5,20 @@
  */
 package com.utn.simulador.negocio.simuladornegocio.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class DecisionesRestController {
 
     @GetMapping("/proyecto/{id}/decisiones")
     public String decisionesPorProyecto() {
 
-        return "[ { id: 1, descripcion: 'Cuanto quiere invertir en publicidad?', opcionSeleccionada: 2, opciones: [	 { id: 1, descripcion: 'Invertir $2000', consecuencias: [{ cuenta: 'Caja', valor: 20 }, { cuenta: 'Stock', valor: 20 }, { cuenta: 'Ventas', valor: 20 }, { cuenta: 'Caja', valor: 20 }] }, { id:2, descripcion: 'Invertir $3000' }, { id:3, descripcion: 'Invertir $4000' }	 ] }, { id: 2, descripcion: 'Cuanto quiere invertir en cosas?', opciones: [{ id: 1, descripcion: 'Invertir $2000' }, { id: 2, descripcion: 'Invertir $3000' }, { id: 3, descripcion: 'Invertir $4000' }] }, { id: 3, descripcion: 'Cuanto quiere invertir en comida para perro?', opciones: [{ id: 1, descripcion: 'Invertir $2000' }, { id: 2, descripcion: 'Invertir $3000' }, { id: 3, descripcion: 'Invertir $4000' }] }, { id: 3, descripcion: 'Cuanto quiere invertir en otras cosas?', opciones: [{ id: 1, descripcion: 'Invertir $2000' }, { id: 2, descripcion: 'Invertir $3000' }, { id: 3, descripcion: 'Invertir $4000' }] } ]";
+        return "[{\"id\":1,\"descripcion\":\"Cuanto quiere invertir en publicidad?\",\"opcionSeleccionada\":2,\"opciones\":[{\"id\":1,\"descripcion\":\"Invertir $2000\",\"consecuencias\":[{\"cuenta\":\"Caja\",\"valor\":20},{\"cuenta\":\"Stock\",\"valor\":20},{\"cuenta\":\"Ventas\",\"valor\":20},{\"cuenta\":\"Caja\",\"valor\":20}]},{\"id\":2,\"descripcion\":\"Invertir $3000\"},{\"id\":3,\"descripcion\":\"Invertir $4000\"}]},{\"id\":2,\"descripcion\":\"Cuanto quiere invertir en cosas?\",\"opciones\":[{\"id\":1,\"descripcion\":\"Invertir $2000\"},{\"id\":2,\"descripcion\":\"Invertir $3000\"},{\"id\":3,\"descripcion\":\"Invertir $4000\"}]},{\"id\":3,\"descripcion\":\"Cuanto quiere invertir en comida para perro?\",\"opciones\":[{\"id\":1,\"descripcion\":\"Invertir $2000\"},{\"id\":2,\"descripcion\":\"Invertir $3000\"},{\"id\":3,\"descripcion\":\"Invertir $4000\"}]},{\"id\":3,\"descripcion\":\"Cuanto quiere invertir en otras cosas?\",\"opciones\":[{\"id\":1,\"descripcion\":\"Invertir $2000\"},{\"id\":2,\"descripcion\":\"Invertir $3000\"},{\"id\":3,\"descripcion\":\"Invertir $4000\"}]}]";
     }
 
 }
