17f538e4e2811afac0cab5164e8d4cd033775824
shgarcia <shgarcia@tgvamericas.net>
2019-09-08 13:59:46 -0400

    get all escenarios

---
+++ src/main/java/com/utn/simulador/negocio/simuladornegocio/controller/EscenariosController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utn.simulador.negocio.simuladornegocio.controller;
+
+import com.utn.simulador.negocio.simuladornegocio.domain.Escenario;
+import com.utn.simulador.negocio.simuladornegocio.service.EscenarioService;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@CrossOrigin
+public class EscenariosController {
+
+    private final EscenarioService escenarioService;
+
+    @GetMapping("/escenarios")
+    public List<Escenario> getEscenarios() {
+        return escenarioService.getEscenarios();
+    }
+
+}
---
+++ src/main/java/com/utn/simulador/negocio/simuladornegocio/service/EscenarioService.java
@@ -0,0 +1,33 @@
+package com.utn.simulador.negocio.simuladornegocio.service;
+
+import com.utn.simulador.negocio.simuladornegocio.domain.Cuenta;
+import com.utn.simulador.negocio.simuladornegocio.domain.Decision;
+import com.utn.simulador.negocio.simuladornegocio.domain.Escenario;
+import com.utn.simulador.negocio.simuladornegocio.domain.Estado;
+import com.utn.simulador.negocio.simuladornegocio.domain.Opcion;
+import com.utn.simulador.negocio.simuladornegocio.domain.OpcionProyecto;
+import com.utn.simulador.negocio.simuladornegocio.domain.Proyecto;
+import com.utn.simulador.negocio.simuladornegocio.repository.DecisionRepository;
+import com.utn.simulador.negocio.simuladornegocio.repository.EscenarioRepository;
+import com.utn.simulador.negocio.simuladornegocio.repository.EstadoRepository;
+import com.utn.simulador.negocio.simuladornegocio.repository.OpcionProyectoRepository;
+import com.utn.simulador.negocio.simuladornegocio.repository.OpcionRepository;
+import com.utn.simulador.negocio.simuladornegocio.repository.ProyectoRepository;
+import com.utn.simulador.negocio.simuladornegocio.vo.DecisionVo;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+@Service
+@RequiredArgsConstructor
+public class EscenarioService {
+
+    private final EscenarioRepository escenarioRepository;
+
+    public List<Escenario> getEscenarios() {
+        return escenarioRepository.findAll();
+    }
+
+}
