4365733248d17e1cab1191a1cdfb09f056465d08
pviviani <pablo.viviani@oracle.com>
2019-09-08 18:55:41 -0300

    simulacion con delete estado anterior

--- src/main/java/com/utn/simulador/negocio/simuladornegocio/service/SimuladorService.java
+++ src/main/java/com/utn/simulador/negocio/simuladornegocio/service/SimuladorService.java
@@ -1,8 +1,11 @@
 package com.utn.simulador.negocio.simuladornegocio.service;
 
 import com.utn.simulador.negocio.simuladornegocio.domain.Estado;
+import com.utn.simulador.negocio.simuladornegocio.repository.ProyectoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.utn.simulador.negocio.simuladornegocio.domain.Proyecto;
+import com.utn.simulador.negocio.simuladornegocio.repository.EstadoRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -11,16 +14,28 @@ public class SimuladorService {
     private final EstadoService estadoService;
     private final SimuladorVentasService simuladorVentasService;
     private final SimuladorProduccionService simuladorProduccionService;
+    private final EstadoRepository estadoRepository;
+    private final ProyectoRepository proyectoRepository;
 
     public Estado simularPeriodo(long proyectoId) {
         Estado estado = avanzarTiempo();
-
         
         simuladorProduccionService.simular(estado);
         simuladorVentasService.simular(estado);
 
         estadoService.guardar(estado);
         return estado;
+    }
+    
+    public void crearPrimerEstadoSimulacion(Long proyectoId) {
+        
+        Proyecto proyecto = proyectoRepository.findById(proyectoId).get();
+        //Estado estado = proyecto.getEscenario().getEstado();
+        
+        //estado.setId(null); //Esto me crea una copia del estado inicial del escenario
+        //estado.setProyecto(proyecto);
+        
+        //estadoService.guardar(estado);
     }
 
     private Estado avanzarTiempo() {
