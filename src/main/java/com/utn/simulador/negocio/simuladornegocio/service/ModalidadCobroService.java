38f76e2cfb967d3356f70b8997b57338ceca4101
pviviani <pablo.viviani@oracle.com>
2019-09-15 18:06:03 -0300

    endpoint modalidad cobro

---
+++ src/main/java/com/utn/simulador/negocio/simuladornegocio/service/ModalidadCobroService.java
@@ -0,0 +1,28 @@
+package com.utn.simulador.negocio.simuladornegocio.service;
+
+import com.utn.simulador.negocio.simuladornegocio.domain.ModalidadCobro;
+import com.utn.simulador.negocio.simuladornegocio.repository.ModalidadCobroRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ModalidadCobroService {
+
+    private final ModalidadCobroRepository modalidadCobroRepository;
+
+    public void guardar(List<ModalidadCobro> listaModalidadesCobro){
+        modalidadCobroRepository.saveAll(listaModalidadesCobro);
+    }
+
+    public void eliminarViejasModalidadesCobro(Long idProyecto){
+        modalidadCobroRepository.deleteByProyectoId(idProyecto);
+    }
+
+    public ModalidadCobro obtenerModalidadesCobro(Long idProyecto){
+        return modalidadCobroRepository.findByProyectoId(idProyecto);
+    }
+
+}
