0304e86f4ede5b8ed258fd10ccf74d43ab1b079e
Laureano Clausi <laureano.clausi@gmail.com>
2019-09-29 15:18:23 -0300

    endpoint para obtener el id del usuario

---
+++ src/main/java/com/utn/simulador/negocio/simuladornegocio/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.utn.simulador.negocio.simuladornegocio.repository;
+
+import com.utn.simulador.negocio.simuladornegocio.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByEmail(String mail);
+
+}
---
+++ src/main/java/com/utn/simulador/negocio/simuladornegocio/service/UsuarioService.java
@@ -0,0 +1,29 @@
+package com.utn.simulador.negocio.simuladornegocio.service;
+
+import com.utn.simulador.negocio.simuladornegocio.domain.Usuario;
+import com.utn.simulador.negocio.simuladornegocio.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public Usuario obtenerPorEmail(String mail) {
+
+        Usuario usuario = usuarioRepository.findByEmail(mail);
+
+        if (usuario == null) {
+            usuario = new Usuario();
+            usuario.setMail(mail);
+            usuario = usuarioRepository.save(usuario);
+        }
+
+        return usuario;
+    }
+
+}
