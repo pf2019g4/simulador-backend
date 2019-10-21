ea0c9a1e3407b6c8f66b033fc8b6f491235da885
fede22 <fede22>
2019-10-20 22:13:24 -0300

    Extender Estado desde EstadoInicial

--- src/main/java/com/utn/simulador/negocio/simuladornegocio/domain/Estado.java
+++ src/main/java/com/utn/simulador/negocio/simuladornegocio/domain/Estado.java
@@ -2,20 +2,17 @@ package com.utn.simulador.negocio.simuladornegocio.domain;
 
 import javax.persistence.*;
 
-import lombok.Data;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-
 @Entity
 @Data
-@Builder(toBuilder = true)
+@SuperBuilder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
-public class Estado {
+public class Estado extends EstadoInicial {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,12 +23,6 @@ public class Estado {
     //TODO borrar producto y la clase/tabla, no se usa para nada
     @ManyToOne
     private Producto producto;
-
-    private BigDecimal costoFijo;
-    private BigDecimal costoVariable;
-    private Long produccionMensual;
-    private Long stock;
-    private Integer calidad;
 
     private BigDecimal caja;
     private BigDecimal ventas;
--- src/main/java/com/utn/simulador/negocio/simuladornegocio/domain/EstadoInicial.java
+++ src/main/java/com/utn/simulador/negocio/simuladornegocio/domain/EstadoInicial.java
@@ -1,19 +1,19 @@
 package com.utn.simulador.negocio.simuladornegocio.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Embeddable;
+import javax.persistence.MappedSuperclass;
 import java.math.BigDecimal;
 
 
 @Data
-@Builder
+@SuperBuilder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Embeddable
+@MappedSuperclass
 public class EstadoInicial {
     private BigDecimal costoFijo;
     private BigDecimal costoVariable;
